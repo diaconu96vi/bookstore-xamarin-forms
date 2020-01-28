77de63a172f36dd664b6554be5a477025ab13948
netrom <diaconu.ionut96@gmail.com>
2020-01-28 18:04:49 +0200

    Make book detail page with some bindings

---
+++ Bookstore/Bookstore/ViewModels/DetailPages/BookDetailPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Bookstore.ViewModels.DetailPages
+{
+    public class BookDetailPageViewModel : BaseViewModel
+    {
+        public BookView ActiveBook { get; set; }
+
+        public string TopLabel { get; set; }
+
+        public ImageSource BookImage { get; set; }
+
+        public BookDetailPageViewModel(BookView bookView)
+        {
+            ActiveBook = bookView;
+            TopLabel = string.Format("{0} - {1}", ActiveBook.AuthorName, ActiveBook.Title);
+            BookImage = ActiveBook.Image;
+            OnPropertyChanged(nameof(TopLabel));
+            OnPropertyChanged(nameof(BookImage));
+        }
+    }
+}
